4e63d77d446fba4c5453ea5f69b1c3c8f60ccd5c
Ratnesh <ratnesh@leftshift.io>
2016-04-26 21:08:53 +0530

    Completed first lesson of react woohoo

--- views/index.jsx
+++ views/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-export default class TodoBox extends React.Component {
-    render() {
-	    return <div className="todoBox">Hello world!</div>
-	}
+export default class TodoBox extends React.Component{
+   render() {
+      return (
+	<div className="todoBox">
+          Hello, world!
+        </div>
+	)
+     }
 }
