a2a8cdb0ec80c639f31438ae26dfcbdce38320fd
Ratnesh <ratnesh@leftshift.io>
2016-04-28 18:56:29 +0530

    done proptypes lesson

--- views/index.jsx
+++ views/index.jsx
@@ -1,61 +1,53 @@
 import React from 'react';
 
-export default class TodoBox extends React.Component{
-   render() {
-      return (
-        <div className="todoBox">
-          <h1>Todos</h1>
-          <TodoList />
-          <TodoForm />
-        </div>
-      )
-  }
+export default class TodoBox extends React.Component {
+    render() {
+        return (
+            <div className="todoBox">
+                <h1>Todos</h1>
+                <TodoList />
+                <TodoForm />
+            </div>
+        );
+    }
 }
 
 class TodoList extends React.Component {
- render ()
- {
-    return (
-      <div className="todoList">
-        <table styles={{border: "2px solid black"}}>
-          <tbody>
-            <Todo title="Shopping">Milk</Todo>
-            <Todo title="Hair cut">13:00</Todo>
-          </tbody>
-        </table>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="todoList">
+              <table style={{border: "2px solid black"}}>
+                <tbody>
+                  <Todo title="Shopping">Milk</Todo>
+                  <Todo title="Hair cut">13:00</Todo>
+                  <Todo title="Learn React">15:00</Todo>
+                </tbody>
+              </table>
+            </div>
+        );
+    }
 }
 
 class Todo extends React.Component {
-  //Write code here
-  render() {
-    return (
-      <div className="todoList">
-        <table styles="border:2px solid black;">
-          <tbody>
+    render() {
+        return (
             <tr>
-              <td styles="border: 1px solid black;">{this.props.title}</td>
-              <td styles="border: 1px solid black;">{this.props.children}</td>
+                <td style={{border: "1px solid black"}}>{this.props.title}</td>
+                <td style={{border: "1px solid black"}}>{this.props.children}</td>
             </tr>
-            <tr>
-              <td styles="border: 1px solid black;">{this.props.title}</td>
-              <td styles="border: 1px solid black;">{this.props.children}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    )
-  }
+        );
+    }
 }
+Todo.propTypes = {
+    title: React.PropTypes.string.isRequired
+};
 
 class TodoForm extends React.Component {
- // Write code here
- render()
- {
-    return (
-      <div className="todoForm">I am a TodoForm.</div>
-    )
-  }
+    render() {
+        return (
+            <div className="todoForm">
+                I am a TodoForm.
+            </div>
+        );
+    }
 }
