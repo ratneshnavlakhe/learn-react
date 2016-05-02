17461090619b9c583cd675580ba0724272a07398
ratneshnavlakhe <ratnesh061288@gmail.com>
2016-05-02 09:09:55 +0530

    work on isomorphic change

--- program.js
+++ program.js
@@ -14,9 +14,18 @@ require('babel/register')({
 
 var TodoBox = require('./views/index.jsx');
 
-var express = require('express');
-var app = express();
-
+app.use('/bundle.js', function (req, res) {
+	res.setHeader('Content-type', 'application/javascript');
+	
+	browserify({ debug: true })
+		.transform(babelify.configure({
+			presets: ["react", "es2015"],
+			compact: false
+		}))
+		.require("./app.js", { entry: true })
+		.bundle()
+		.pipe(res);
+})
 
 app.use('/', function (req, res) {
   var initialData = JSON.stringify(data);
@@ -41,18 +50,8 @@ app.set('view engine', 'jsx');
 app.set('views', __dirname + '/views');
 app.engine('jsx', require('express-react-views').createEngine({ transformViews: false }));
 
-require('babel/register')({
-    ignore: false
-});
-
-var data = [];
-
-app.use('/', function(req, res) {
-  res.render('index', {data: data});
-});
-
 app.listen(app.get('port'), function() {
-data.push({ title: 'Shopping' , 'detail': process.argv[3]});
-data.push({ title: 'Hair cut', 'detail': process.argv[4]});
+//data.push({ title: 'Shopping' , 'detail': process.argv[3]});
+//data.push({ title: 'Hair cut', 'detail': process.argv[4]});
 console.log('listening on port 3000');
 });
